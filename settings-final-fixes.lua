bb43e58543233a52c69d16e03ef7087dbf6f7401
Em Karisch <emii.khaos@gmail.com>
2026-07-21 15:06:40 +0200

    fix: correctly force extracted settings into the transfer setting

--- settings-final-fixes.lua
+++ settings-final-fixes.lua
@@ -1,11 +1,14 @@
 local serializer = require("__khaosdata-extractor__.common.serializer")
 
+local extracted_settings = serializer.serialize(data.raw) .. "\n"
+
 data.extend({
   {
     type = "string-setting",
     name = "khaosdata-extractor-extracted-settings",
     setting_type = "startup",
-    default_value = serializer.serialize(data.raw) .. "\n",
+    default_value = extracted_settings,
+    allowed_values = {extracted_settings},
     hidden = true,
   }
 })
